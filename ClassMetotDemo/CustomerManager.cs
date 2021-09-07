c2ba8d19634eb3fa387a0ada0f0b576c956ecd4e
DELL <DELL@DESKTOP-4DH1NF6>
2021-09-07 11:11:51 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager 
+    {
+        public void Ekle(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi={0}", customer.MusteriAdi);
+        
+        
+        }
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi={0}", customer.MusteriAdi);
+
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.MusteriAdi = "Samet";
+            customer1.MusteriSoyadi = "KILIÇ";
+            customer1.IbanNo = 123456;
+
+            CustomerManager cmanager = new CustomerManager();
+            cmanager.Ekle(customer1);
+            cmanager.Sil(customer1);
+
+
+
+
+
+
+
+
+
+
+        }
+
+        
+      
+        
+    }
+}
